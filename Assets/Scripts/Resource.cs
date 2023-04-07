4466a3f2fb4b6613eb364d10cba9e5083b00df0d
PiccioneAnna <ajp7549@rit.edu>
2023-04-07 13:30:36 -0400

    Updated sprites and scenes

--- Assets/Scripts/Resource.cs
+++ Assets/Scripts/Resource.cs
@@ -7,20 +7,27 @@ public class Resource : GameObject
 {
     public Resource instance;
     public Item properTool;
+    public int maxDropCount = 5;
+    private int dropCount;
 
     public Item[] drops;
+    public Sprite[] sprites;
     public int health;
     public UnityEngine.GameObject[] droppedObjs;
 
     Vector3 position;
     Quaternion rotation;
-    
+
     void Awake()
     {
         position = transform.position;
         rotation = transform.rotation;
 
-        DontDestroyOnLoad(this.gameObject);
+        System.Random random = new System.Random();
+
+        instance.transform.GetComponent<SpriteRenderer>().sprite = sprites[random.Next(sprites.Length)];
+
+        dropCount = random.Next(5);
     }
 
     public void Shake()
@@ -33,12 +40,19 @@ public class Resource : GameObject
         Shake();
         health--;
         Debug.Log(health);
+        System.Random random = new System.Random();
+        float offset = (float)random.NextDouble()/2;
+        float multplier = offset % 2 == 2 ? 1 : -1;
 
-        if(health <= 0)
+        if (health <= 0)
         {
             foreach (UnityEngine.GameObject drop in droppedObjs)
             {
-                Instantiate(drop, position, rotation);
+                for (int i = 0; i < dropCount; i++)
+                {
+                    position = new Vector3(position.x, position.y + (multplier * offset), position.z);
+                    Instantiate(drop, position, rotation);
+                }
             }
             Destroy(instance.gameObject);
         }
